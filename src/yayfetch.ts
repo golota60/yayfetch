ca42fc1f2c671580ea71a12e52aa8e86b947e598
Szymon Wiszczuk <golota60@gmail.com>
2020-07-08 20:55:19 +0200

    moved yayfetch logo to display next to specs

--- src/yayfetch.ts
+++ src/yayfetch.ts
@@ -6,7 +6,8 @@ import os from 'os';
 import yargs from 'yargs';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import { uptimeInMinutes, returnInPink, yayfetchASCII } from './helpers/helpers';
+import columnify from 'columnify';
+import { uptimeInMinutes, returnInPink, yayfetchASCII, printInTwoColumns } from './helpers/helpers';
 import {
   getEndianness,
   getDisplaysAndGraphicsCards,
@@ -43,46 +44,42 @@ const getSystemInformation = async (): Promise<SystemInformation> => ({
   shellInfo: await getShellInfo(),
 });
 
-async function displayAllData(): Promise<void> {
+const allData = async (): Promise<string> => {
   const allData: SystemInformation = await getSystemInformation();
-  allData
-    ? console.log(
-        ` ${returnInPink(allData.osInfo.username + '@' + os.platform())} \n -----------------------------\n`,
-        `${returnInPink(`Platform:`)} ${os.platform().toLocaleUpperCase()}\n`,
-        `${returnInPink(`Type:`)} ${os.type()}\n`,
-        `${returnInPink(`Release:`)} ${os.release()}\n`,
-        `${returnInPink(`Architecture:`)} ${os.arch()}\n`,
-        `${returnInPink(`Uptime:`)} ${uptimeInMinutes().toFixed(0)} min\n`,
-        `${returnInPink(`CPU:`)} ${os.cpus()[0].model}\n`, //supports only one cpu
-        `${returnInPink(`GPU(s):`)} ${allData.graphicsInfo.gpuInfo}\n`,
-        `${returnInPink(`Display(s):`)} ${allData.graphicsInfo.displays}\n`,
-        `${returnInPink(`Endianness:`)} ${getEndianness()}\n`,
-        `${returnInPink(`Memory:`)} ${allData.memoryInfo.free}/${allData.memoryInfo.used}/${
-          allData.memoryInfo.total
-        } MiB (Free/Used/Total)\n`,
-        `${returnInPink(`Shell:`)} ${allData.shellInfo}`,
-      )
-    : null;
-}
+  return ` ${returnInPink(allData.osInfo.username + '@' + os.platform())} \n -----------------------------\n
+  ${returnInPink(`Platform:`)} ${os.platform().toLocaleUpperCase()}\n
+  ${returnInPink(`Type:`)} ${os.type()}\n
+  ${returnInPink(`Release:`)} ${os.release()}\n
+  ${returnInPink(`Architecture:`)} ${os.arch()}\n
+  ${returnInPink(`Uptime:`)} ${uptimeInMinutes().toFixed(0)} min\n
+  ${returnInPink(`CPU:`)} ${os.cpus()[0].model}\n
+  ${returnInPink(`GPU(s):`)} ${allData.graphicsInfo.gpuInfo}\n
+  ${returnInPink(`Display(s):`)} ${allData.graphicsInfo.displays}\n
+  ${returnInPink(`Endianness:`)} ${getEndianness()}\n
+  ${returnInPink(`Memory:`)} ${allData.memoryInfo.free}/${allData.memoryInfo.used}/${
+    allData.memoryInfo.total
+  } MiB (Free/Used/Total)\n
+  ${returnInPink(`Shell:`)} ${allData.shellInfo}`;
+};
 
-async function displayPickedData(valuesToDisplay: Array<string>): Promise<void> {
+async function returnPickedData(valuesToDisplay: Array<string>): Promise<string> {
   const allData: SystemInformation = await getSystemInformation();
-  console.log(` ${chalk.blue(allData.osInfo.username + '@' + os.platform())} \n -----------------------------`);
-  valuesToDisplay.includes('Platform') && console.log(`Platform: ${os.platform().toLocaleUpperCase()}`);
-  valuesToDisplay.includes('Type') && console.log(`Type: ${os.type()}`);
-  valuesToDisplay.includes('Release') && console.log(`Release: ${os.release()}`);
-  valuesToDisplay.includes('Architecture') && console.log(`Architecture: ${os.arch()}`);
-  valuesToDisplay.includes('Uptime') && console.log(`Uptime: ${uptimeInMinutes().toFixed(0)} min`);
-  valuesToDisplay.includes('CPUs') && console.log(`CPU: ${os.cpus()[0].model}`);
-  valuesToDisplay.includes('GPUs') && console.log(`GPU(s): ${allData.graphicsInfo.gpuInfo}`);
-  valuesToDisplay.includes('Displays') && console.log(`Display(s): ${allData.graphicsInfo.displays}`);
-  valuesToDisplay.includes('Endianness') && console.log(`Endianness: ${getEndianness()}`);
+  const pickedVals = [`${chalk.blue(allData.osInfo.username + '@' + os.platform())} \n -----------------------------`];
+  valuesToDisplay.includes('Platform') && pickedVals.push(`Platform: ${os.platform().toLocaleUpperCase()}`);
+  valuesToDisplay.includes('Type') && pickedVals.push(`Type: ${os.type()}`);
+  valuesToDisplay.includes('Release') && pickedVals.push(`Release: ${os.release()}`);
+  valuesToDisplay.includes('Architecture') && pickedVals.push(`Architecture: ${os.arch()}`);
+  valuesToDisplay.includes('Uptime') && pickedVals.push(`Uptime: ${uptimeInMinutes().toFixed(0)} min`);
+  valuesToDisplay.includes('CPUs') && pickedVals.push(`CPU: ${os.cpus()[0].model}`);
+  valuesToDisplay.includes('GPUs') && pickedVals.push(`GPU(s): ${allData.graphicsInfo.gpuInfo}`);
+  valuesToDisplay.includes('Displays') && pickedVals.push(`Display(s): ${allData.graphicsInfo.displays}`);
+  valuesToDisplay.includes('Endianness') && pickedVals.push(`Endianness: ${getEndianness()}`);
   valuesToDisplay.includes('Memory') &&
-    console.log(
+    pickedVals.push(
       `Memory: ${allData.memoryInfo.free}/${allData.memoryInfo.used}/${allData.memoryInfo.total} MiB (Free/Used/Total)`,
     );
-  valuesToDisplay.includes('Shell') && console.log(`Shell: ${allData.shellInfo}`);
-  return;
+  valuesToDisplay.includes('Shell') && pickedVals.push(`Shell: ${allData.shellInfo}`);
+  return pickedVals.join('\n');
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -90,12 +87,9 @@ const args = yargs
   .command('$0', '', async () => {
     if (yargs.argv.p || yargs.argv.pick) {
       const inquirerPrompt = await inquirer.prompt<{ displayedValues: Array<string> }>([promptQuestions]);
-      console.log(returnInPink(yayfetchASCII));
-      displayPickedData(inquirerPrompt.displayedValues);
+      printInTwoColumns(returnInPink(yayfetchASCII), await returnPickedData(inquirerPrompt.displayedValues));
     } else {
-      const splitlogo = yayfetchASCII.split('\n');
-      console.log(returnInPink(yayfetchASCII));
-      displayAllData();
+      printInTwoColumns(returnInPink(yayfetchASCII), await allData());
     }
   })
   .scriptName('')
