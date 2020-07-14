2d6f152f56b1377d589d0073370c4efd9e2f1960
golota60 <37072867+golota60@users.noreply.github.com>
2020-07-14 22:07:31 +0200

    Print yayfetch logo (#24)
    * moved yayfetch logo to display next to specs
    
    * printing yayfetch logo

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,5 +1,6 @@
 import os from 'os';
 import chalk from 'chalk';
+import columnify from 'columnify';
 
 export const errorMessage = 'Error - check https://www.npmjs.com/package/yayfetch for more';
 
@@ -9,9 +10,24 @@ export const uptimeInMinutes = (): number => os.uptime() / 60;
 
 export const returnInPink = (textToPrint: string): string => chalk.rgb(255, 102, 147)(textToPrint);
 
+export const printInTwoColumns = (col1: string, col2: string): void => {
+  const data = [
+    {
+      logo: col1,
+      specs: col2,
+    },
+  ];
+  console.log(
+    columnify(data, {
+      preserveNewLines: true,
+      config: { logo: { showHeaders: false }, specs: { showHeaders: false } },
+    }),
+  );
+}
+
 export const yayfetchASCII = `
   ████                  ████████                 ████
- ██████                ██████████              ███████
+ ███████               ██████████              ███████
   ███████             ████████████            ███████
    ███████          ███████████████          ███████
     ███████        █████████████████        ███████
