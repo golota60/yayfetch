a843b9e09498ce157780c4de403c9d7ace84b2fa
Szymon Wiszczuk <37072867+golota60@users.noreply.github.com>
2021-09-21 23:16:56 +0200

    chore: remove xo linter and replace it with callstacks config (#68)

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,36 +1,35 @@
-import os from 'os';
-import chalk from 'chalk';
-import {RGBColors} from '../interfaces/general';
+import os from "os";
+import chalk from "chalk";
+import { RGBColors } from "../interfaces/general";
 
 export const errorMessage =
-  'Error - check https://www.npmjs.com/package/yayfetch or https://github.com/golota60/yayfetch for more';
+  "Error - check https://www.npmjs.com/package/yayfetch or https://github.com/golota60/yayfetch for more";
 
 export const availableColors = [
-  'pink',
-  'orange',
-  'green',
-  'white',
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'violet',
-  'rainbow'
+  "pink",
+  "orange",
+  "green",
+  "white",
+  "black",
+  "red",
+  "blue",
+  "yellow",
+  "violet",
+  "rainbow",
 ];
 
 export type PredefinedColors = typeof availableColors[number];
 
 export const bitstoMegabytes = (numberToConvert: number): number =>
-  /* eslint-disable-next-line no-mixed-operators */
   numberToConvert * 9.537 * 10 ** -7;
 
 export const parseRGBStringToNumber = (rgbString: string): RGBColors => {
-  const split = rgbString.split(',');
+  const split = rgbString.split(",");
   const RGBAsNumericalArray = split.map((color: string) => {
     const colorNumber = Number(color);
     if (Number.isNaN(colorNumber) || colorNumber < 0 || colorNumber > 255) {
       throw new Error(
-        'One of the numbers wasn\'t provided in correct format(has to be a number between 0 and 255)'
+        "One of the numbers wasn't provided in correct format(has to be a number between 0 and 255)"
       );
     }
 
@@ -38,65 +37,65 @@ export const parseRGBStringToNumber = (rgbString: string): RGBColors => {
   });
   if (split.length !== 3) {
     throw new Error(
-      'Specified RGB color was provided in incorrect form. Please remember that there has to be exactly 3 colors, they need to be separated by comas and numbers must be between 0 and 255'
+      "Specified RGB color was provided in incorrect form. Please remember that there has to be exactly 3 colors, they need to be separated by comas and numbers must be between 0 and 255"
     );
   }
 
   return {
     r: RGBAsNumericalArray[0],
     g: RGBAsNumericalArray[1],
-    b: RGBAsNumericalArray[2]
+    b: RGBAsNumericalArray[2],
   };
 };
 
 const customColorCodes = {
-  pink: {r: 255, g: 102, b: 147},
-  orange: {r: 255, g: 170, b: 16},
-  yellow: {r: 255, g: 245, b: 99},
-  violet: {r: 186, g: 13, b: 255},
-  lightgrey: {r: 224, g: 224, b: 224},
-  darkgrey: {r: 152, g: 152, b: 152},
-  burgundy: {r: 146, g: 36, b: 36},
-  darkgreen: {r: 63, g: 163, b: 38},
-  lightgreen: {r: 95, g: 235, b: 61},
-  darkyellow: {r: 179, g: 203, b: 0},
-  deepBlue: {r: 0, g: 47, b: 255},
-  darkDeepBlue: {r: 0, g: 30, b: 161},
-  darkViolet: {r: 181, g: 47, b: 191},
-  darkCyan: {r: 0, g: 171, b: 186}
+  pink: { r: 255, g: 102, b: 147 },
+  orange: { r: 255, g: 170, b: 16 },
+  yellow: { r: 255, g: 245, b: 99 },
+  violet: { r: 186, g: 13, b: 255 },
+  lightgrey: { r: 224, g: 224, b: 224 },
+  darkgrey: { r: 152, g: 152, b: 152 },
+  burgundy: { r: 146, g: 36, b: 36 },
+  darkgreen: { r: 63, g: 163, b: 38 },
+  lightgreen: { r: 95, g: 235, b: 61 },
+  darkyellow: { r: 179, g: 203, b: 0 },
+  deepBlue: { r: 0, g: 47, b: 255 },
+  darkDeepBlue: { r: 0, g: 30, b: 161 },
+  darkViolet: { r: 181, g: 47, b: 191 },
+  darkCyan: { r: 0, g: 171, b: 186 },
 };
 
 export const uptimeInMinutes = (): number => os.uptime() / 60;
 
 export const returnInPink = (text: string, bolded = false): string =>
-  bolded ?
-    chalk
-      .rgb(
+  bolded
+    ? chalk
+        .rgb(
+          customColorCodes.pink.r,
+          customColorCodes.pink.g,
+          customColorCodes.pink.b
+        )
+        .bold(text)
+    : chalk.rgb(
         customColorCodes.pink.r,
         customColorCodes.pink.g,
         customColorCodes.pink.b
-      )
-      .bold(text) :
-    chalk.rgb(
-      customColorCodes.pink.r,
-      customColorCodes.pink.g,
-      customColorCodes.pink.b
-    )(text);
+      )(text);
 
 export const returnInOrange = (text: string, bolded = false): string =>
-  bolded ?
-    chalk
-      .rgb(
+  bolded
+    ? chalk
+        .rgb(
+          customColorCodes.orange.r,
+          customColorCodes.orange.g,
+          customColorCodes.orange.b
+        )
+        .bold(text)
+    : chalk.rgb(
         customColorCodes.orange.r,
         customColorCodes.orange.g,
         customColorCodes.orange.b
-      )
-      .bold(text) :
-    chalk.rgb(
-      customColorCodes.orange.r,
-      customColorCodes.orange.g,
-      customColorCodes.orange.b
-    )(text);
+      )(text);
 
 export const returnInGreen = (text: string, bolded = false): string =>
   bolded ? chalk.green.bold(text) : chalk.green(text);
@@ -114,34 +113,34 @@ export const returnInBlue = (text: string, bolded = false): string =>
   bolded ? chalk.blue.bold(text) : chalk.blue(text);
 
 export const returnInYellow = (text: string, bolded = false): string =>
-  bolded ?
-    chalk
-      .rgb(
+  bolded
+    ? chalk
+        .rgb(
+          customColorCodes.yellow.r,
+          customColorCodes.yellow.g,
+          customColorCodes.yellow.b
+        )
+        .bold(text)
+    : chalk.rgb(
         customColorCodes.yellow.r,
         customColorCodes.yellow.g,
         customColorCodes.yellow.b
-      )
-      .bold(text) :
-    chalk.rgb(
-      customColorCodes.yellow.r,
-      customColorCodes.yellow.g,
-      customColorCodes.yellow.b
-    )(text);
+      )(text);
 
 export const returnInViolet = (text: string, bolded = false): string =>
-  bolded ?
-    chalk
-      .rgb(
+  bolded
+    ? chalk
+        .rgb(
+          customColorCodes.violet.r,
+          customColorCodes.violet.g,
+          customColorCodes.violet.b
+        )
+        .bold(text)
+    : chalk.rgb(
         customColorCodes.violet.r,
         customColorCodes.violet.g,
         customColorCodes.violet.b
-      )
-      .bold(text) :
-    chalk.rgb(
-      customColorCodes.violet.r,
-      customColorCodes.violet.g,
-      customColorCodes.violet.b
-    )(text);
+      )(text);
 
 export const returnInRainbow = (text: string, bolded = false): string => {
   const functionArray = [
@@ -151,14 +150,14 @@ export const returnInRainbow = (text: string, bolded = false): string => {
     returnInGreen,
     returnInBlue,
     returnInViolet,
-    returnInPink
+    returnInPink,
   ];
   const coloredText = text
-    .split('')
+    .split("")
     .map((char, i) => {
       return functionArray[i % functionArray.length](char, bolded);
     })
-    .join('');
+    .join("");
   return coloredText;
 };
 
@@ -167,53 +166,51 @@ export const returnColoredText = (
   colorCode: PredefinedColors | RGBColors,
   bolded = false
 ): string => {
-  if (typeof colorCode === 'object') {
+  if (typeof colorCode === "object") {
     return chalk.rgb(colorCode.r, colorCode.g, colorCode.b)(text);
   }
 
   switch (colorCode) {
-    case 'pink':
+    case "pink":
       return returnInPink(text, bolded);
-    case 'orange':
+    case "orange":
       return returnInOrange(text, bolded);
-    case 'blue':
+    case "blue":
       return returnInBlue(text, bolded);
-    case 'green':
+    case "green":
       return returnInGreen(text, bolded);
-    case 'white':
+    case "white":
       return returnInWhite(text, bolded);
-    case 'black':
+    case "black":
       return returnInBlack(text, bolded);
-    case 'red':
+    case "red":
       return returnInRed(text, bolded);
-    case 'yellow':
+    case "yellow":
       return returnInYellow(text, bolded);
-    case 'violet':
+    case "violet":
       return returnInViolet(text, bolded);
-    case 'rainbow':
+    case "rainbow":
       return returnInRainbow(text, bolded);
     default:
       return returnInPink(text, bolded);
   }
 };
 
-/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-unused-expressions */
 export const printInColumns = (...cols: string[]): void => {
   // Split every line by \n
-  const colsSplit = cols.map(element => element.split('\n'));
+  const colsSplit = cols.map((element) => element.split("\n"));
   // Find the vertically longest argument
   // Length of which is going the be the iterator on how many times we have to print a line
-  // eslint-disable-next-line unicorn/no-array-reduce
   const verticallyLongestArg = colsSplit.reduce(
     (acc, value) => (value.length > acc ? value.length : acc),
     0
   );
   const argsNumber = cols.length;
   const mergedArgs = [] as string[];
-  for (const [i, _] of [...new Array(verticallyLongestArg)].entries()) {
-    let nextLine = '';
-    for (const [i2, _2] of [...new Array(argsNumber)].entries()) {
-      if (nextLine === '') {
+  for (const [i] of [...new Array(verticallyLongestArg)].entries()) {
+    let nextLine = "";
+    for (const [i2] of [...new Array(argsNumber)].entries()) {
+      if (nextLine === "") {
         colsSplit[i2];
       }
 
@@ -224,12 +221,11 @@ export const printInColumns = (...cols: string[]): void => {
   }
 
   // TODO: Improve the algorithm so that it auto adjusts the amount of spaces in every row
-  console.log(mergedArgs.join('\n'));
+  console.log(mergedArgs.join("\n"));
 };
-/* eslint-enable @typescript-eslint/no-unused-vars, @typescript-eslint/no-unused-expressions */
 
 export const printData = (
-  {logo, data}: {logo: string; data: string},
+  { logo, data }: { logo: string; data: string },
   hideLogo = false
 ): void => {
   if (hideLogo) {
@@ -240,48 +236,48 @@ export const printData = (
 };
 
 export const getColoredBoxes = () => {
-  return `${chalk.bgBlack('   ')}${chalk.bgRgb(
+  return `${chalk.bgBlack("   ")}${chalk.bgRgb(
     customColorCodes.burgundy.r,
     customColorCodes.burgundy.g,
     customColorCodes.burgundy.b
-  )('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgRgb(
     customColorCodes.darkgreen.r,
     customColorCodes.darkgreen.g,
     customColorCodes.darkgreen.b
-  )('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgRgb(
     customColorCodes.darkyellow.r,
     customColorCodes.darkyellow.g,
     customColorCodes.darkyellow.b
-  )('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgRgb(
     customColorCodes.darkDeepBlue.r,
     customColorCodes.darkDeepBlue.g,
     customColorCodes.darkDeepBlue.b
-  )('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgRgb(
     customColorCodes.darkViolet.r,
     customColorCodes.darkViolet.g,
     customColorCodes.darkViolet.b
-  )('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgRgb(
     customColorCodes.darkCyan.r,
     customColorCodes.darkCyan.g,
     customColorCodes.darkCyan.b
-  )('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgRgb(
     customColorCodes.lightgrey.r,
     customColorCodes.lightgrey.g,
     customColorCodes.lightgrey.b
-  )('   ')}\n${chalk.bgRgb(
+  )("   ")}\n${chalk.bgRgb(
     customColorCodes.darkgrey.r,
     customColorCodes.darkgrey.g,
     customColorCodes.darkgrey.b
-  )('   ')}${chalk.bgRed('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgRed("   ")}${chalk.bgRgb(
     customColorCodes.lightgreen.r,
     customColorCodes.lightgreen.g,
     customColorCodes.lightgreen.b
-  )('   ')}${chalk.bgYellow('   ')}${chalk.bgRgb(
+  )("   ")}${chalk.bgYellow("   ")}${chalk.bgRgb(
     customColorCodes.deepBlue.r,
     customColorCodes.deepBlue.g,
     customColorCodes.deepBlue.b
-  )('   ')}${chalk.bgMagenta('   ')}${chalk.bgCyanBright('   ')}${chalk.bgWhite(
-    '   '
+  )("   ")}${chalk.bgMagenta("   ")}${chalk.bgCyanBright("   ")}${chalk.bgWhite(
+    "   "
   )}`;
 };
 
--- src/yayfetch.ts
+++ src/yayfetch.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-import os from 'os';
-import yargs from 'yargs';
-import inquirer from 'inquirer';
-import {Systeminformation} from 'systeminformation';
+import os from "os";
+import yargs from "yargs";
+import inquirer from "inquirer";
+import { Systeminformation } from "systeminformation";
 import {
   DisplayAndGraphicsCard,
   MemoryInfoInterface,
-  OSInfoInterface
-} from './interfaces/systeminformation';
+  OSInfoInterface,
+} from "./interfaces/systeminformation";
 import {
   uptimeInMinutes,
   yayfetchASCII,
@@ -17,8 +17,8 @@ import {
   printData,
   availableColors,
   PredefinedColors,
-  getColoredBoxes
-} from './helpers/helpers';
+  getColoredBoxes,
+} from "./helpers/helpers";
 import {
   getEndianness,
   getDisplaysAndGraphicsCards,
@@ -26,30 +26,30 @@ import {
   getOsInfo,
   getShellInfo,
   getSysInfOsInfo,
-  getHWInfo
-} from './helpers/systeminformation';
-import {RGBColors} from './interfaces/general';
+  getHWInfo,
+} from "./helpers/systeminformation";
+import { RGBColors } from "./interfaces/general";
 
 export const promptQuestionsChoices = [
-  'OS',
-  'Type',
-  'Release',
-  'Model',
-  'Architecture',
-  'Uptime',
-  'CPUs',
-  'GPUs',
-  'Displays',
-  'Endianness',
-  'Memory',
-  'Shell'
+  "OS",
+  "Type",
+  "Release",
+  "Model",
+  "Architecture",
+  "Uptime",
+  "CPUs",
+  "GPUs",
+  "Displays",
+  "Endianness",
+  "Memory",
+  "Shell",
 ];
 
 const promptQuestions = {
-  type: 'checkbox',
-  name: 'displayedValues',
-  message: 'What information do you need?',
-  choices: promptQuestionsChoices
+  type: "checkbox",
+  name: "displayedValues",
+  message: "What information do you need?",
+  choices: promptQuestionsChoices,
 };
 
 export interface SystemInformation {
@@ -65,9 +65,9 @@ const getSystemInformation = async (): Promise<SystemInformation> => ({
   graphicsInfo: await getDisplaysAndGraphicsCards(),
   memoryInfo: await getMemoryInfo(),
   osInfo: {
-    username: await getOsInfo()
+    username: await getOsInfo(),
   },
-  shellInfo: await getShellInfo()
+  shellInfo: await getShellInfo(),
 });
 
 async function returnPickedData(
@@ -82,77 +82,77 @@ async function returnPickedData(
       `${allData.osInfo.username}@${sysinfOsInfo.hostname}`,
       color,
       true
-    )} \n -----------------------------`
+    )} \n -----------------------------`,
   ];
-  if (valuesToDisplay.includes('OS')) {
+  if (valuesToDisplay.includes("OS")) {
     pickedVals.push(
-      `${returnColoredText('OS:', color, true)} ${sysinfOsInfo.display}`
+      `${returnColoredText("OS:", color, true)} ${sysinfOsInfo.display}`
     );
   }
 
-  if (valuesToDisplay.includes('Type')) {
+  if (valuesToDisplay.includes("Type")) {
     pickedVals.push(
-      `${returnColoredText('Type:', color, true)} ${sysinfOsInfo.distro}`
+      `${returnColoredText("Type:", color, true)} ${sysinfOsInfo.distro}`
     );
   }
 
-  if (valuesToDisplay.includes('Model')) {
+  if (valuesToDisplay.includes("Model")) {
     pickedVals.push(
-      `${returnColoredText('Model:', color, true)} ${hwInfo.model}`
+      `${returnColoredText("Model:", color, true)} ${hwInfo.model}`
     );
   }
 
-  if (valuesToDisplay.includes('Release')) {
+  if (valuesToDisplay.includes("Release")) {
     pickedVals.push(
-      `${returnColoredText('Release:', color, true)} ${os.release()}`
+      `${returnColoredText("Release:", color, true)} ${os.release()}`
     );
   }
 
-  if (valuesToDisplay.includes('Architecture')) {
+  if (valuesToDisplay.includes("Architecture")) {
     pickedVals.push(
-      `${returnColoredText('Architecture:', color, true)} ${os.arch()}`
+      `${returnColoredText("Architecture:", color, true)} ${os.arch()}`
     );
   }
 
-  if (valuesToDisplay.includes('Uptime')) {
+  if (valuesToDisplay.includes("Uptime")) {
     pickedVals.push(
-      `${returnColoredText('Uptime:', color, true)} ${uptimeInMinutes().toFixed(
+      `${returnColoredText("Uptime:", color, true)} ${uptimeInMinutes().toFixed(
         0
       )} min`
     );
   }
 
-  if (valuesToDisplay.includes('CPUs')) {
+  if (valuesToDisplay.includes("CPUs")) {
     pickedVals.push(
-      `${returnColoredText('CPU:', color, true)} ${os.cpus()[0].model}`
+      `${returnColoredText("CPU:", color, true)} ${os.cpus()[0].model}`
     );
   }
 
-  if (valuesToDisplay.includes('GPUs')) {
+  if (valuesToDisplay.includes("GPUs")) {
     pickedVals.push(
-      `${returnColoredText('GPU(s):', color, true)} ${
+      `${returnColoredText("GPU(s):", color, true)} ${
         allData.graphicsInfo.gpuInfo
       }`
     );
   }
 
-  if (valuesToDisplay.includes('Displays')) {
+  if (valuesToDisplay.includes("Displays")) {
     pickedVals.push(
-      `${returnColoredText('Display(s):', color, true)} ${
+      `${returnColoredText("Display(s):", color, true)} ${
         allData.graphicsInfo.displays
       }`
     );
   }
 
-  if (valuesToDisplay.includes('Endianness')) {
+  if (valuesToDisplay.includes("Endianness")) {
     pickedVals.push(
-      `${returnColoredText('Endianness:', color, true)} ${getEndianness()}`
+      `${returnColoredText("Endianness:", color, true)} ${getEndianness()}`
     );
   }
 
-  if (valuesToDisplay.includes('Memory')) {
+  if (valuesToDisplay.includes("Memory")) {
     pickedVals.push(
-      `${returnColoredText('Memory:', color, true)} ${
+      `${returnColoredText("Memory:", color, true)} ${
         allData.memoryInfo.free
       }/${allData.memoryInfo.used}/${
         allData.memoryInfo.total
@@ -160,9 +160,9 @@ async function returnPickedData(
     );
   }
 
-  if (valuesToDisplay.includes('Shell')) {
+  if (valuesToDisplay.includes("Shell")) {
     pickedVals.push(
-      `${returnColoredText('Shell:', color, true)} ${allData.shellInfo}`
+      `${returnColoredText("Shell:", color, true)} ${allData.shellInfo}`
     );
   }
 
@@ -171,22 +171,22 @@ async function returnPickedData(
 
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-implicit-any-catch */
 const _args = yargs
-  .command('$0', '', async () => {
+  .command("$0", "", async () => {
     try {
       if (yargs.argv.color && yargs.argv.rgb) {
         throw new Error(
-          '--rgb and --color are mutually exclusive - please specify only one'
+          "--rgb and --color are mutually exclusive - please specify only one"
         );
       }
 
       const predefinedColor = String(yargs.argv.c || yargs.argv.color);
-      const customColors = yargs.argv.rgb ?
-        parseRGBStringToNumber(String(yargs.argv.rgb)) :
-        false;
+      const customColors = yargs.argv.rgb
+        ? parseRGBStringToNumber(String(yargs.argv.rgb))
+        : false;
       const colorToUse = customColors || predefinedColor;
-      const hideLogoFlag = Boolean(yargs.argv['hide-logo']);
-      const coloredBoxes = Boolean(yargs.argv['colored-boxes']);
-      const customLines = yargs.argv['custom-lines'] as string[];
+      const hideLogoFlag = Boolean(yargs.argv["hide-logo"]);
+      const coloredBoxes = Boolean(yargs.argv["colored-boxes"]);
+      const customLines = yargs.argv["custom-lines"] as string[];
 
       let infoToPrint: string[];
       if (yargs.argv.p || yargs.argv.pick) {
@@ -205,7 +205,7 @@ const _args = yargs
       }
 
       if (customLines) {
-        const customLinesParsed = customLines.map(line =>
+        const customLinesParsed = customLines.map((line) =>
           JSON.parse(line)
         ) as Array<{
           key: string;
@@ -213,23 +213,23 @@ const _args = yargs
         }>;
         infoToPrint = [
           ...infoToPrint,
-          ...customLinesParsed.map(customLine => {
+          ...customLinesParsed.map((customLine) => {
             return `${returnColoredText(customLine.key, colorToUse, true)} ${
               customLine.value
             }`;
-          })
+          }),
         ];
       }
 
       if (coloredBoxes) {
         /* Empty string to ensure space between boxes */
-        infoToPrint = [...infoToPrint, '', getColoredBoxes()];
+        infoToPrint = [...infoToPrint, "", getColoredBoxes()];
       }
 
       printData(
         {
           logo: returnColoredText(yayfetchASCII, colorToUse),
-          data: infoToPrint.join('\n')
+          data: infoToPrint.join("\n"),
         },
         hideLogoFlag
       );
@@ -237,40 +237,40 @@ const _args = yargs
       console.error(`‼️  ${error} ‼️`);
     }
   })
-  .scriptName('')
-  .usage('Usage: npx yayfetch <flags>')
-  .option('p', {
-    alias: 'pick',
-    describe: 'Asks you which information you want displayed',
-    type: 'boolean',
-    default: false
+  .scriptName("")
+  .usage("Usage: npx yayfetch <flags>")
+  .option("p", {
+    alias: "pick",
+    describe: "Asks you which information you want displayed",
+    type: "boolean",
+    default: false,
   })
-  .option('c', {
-    alias: 'color',
-    describe: 'Color in which the data will be printed',
+  .option("c", {
+    alias: "color",
+    describe: "Color in which the data will be printed",
     choices: availableColors,
-    type: 'string'
+    type: "string",
   })
-  .option('hide-logo', {
-    describe: 'Hides the ASCII logo',
-    type: 'boolean',
-    default: false
+  .option("hide-logo", {
+    describe: "Hides the ASCII logo",
+    type: "boolean",
+    default: false,
   })
-  .option('rgb', {
-    describe: 'Same as color, but you provide r,g,b values ex. 128,5,67',
-    type: 'string'
+  .option("rgb", {
+    describe: "Same as color, but you provide r,g,b values ex. 128,5,67",
+    type: "string",
   })
-  .option('custom-lines', {
-    describe: 'Array of lines you want to add(objects with key, value pairs)',
-    type: 'array'
+  .option("custom-lines", {
+    describe: "Array of lines you want to add(objects with key, value pairs)",
+    type: "array",
   })
-  .option('colored-boxes', {
-    describe: 'Hides colored boxes underneath the information',
-    type: 'boolean',
-    default: true
+  .option("colored-boxes", {
+    describe: "Hides colored boxes underneath the information",
+    type: "boolean",
+    default: true,
   })
   .help()
   .version()
-  .alias('help', 'h')
-  .example('npx yayfetch', 'Returns information about your system').argv;
+  .alias("help", "h")
+  .example("npx yayfetch", "Returns information about your system").argv;
 /* eslint-enable @typescript-eslint/no-unused-vars, @typescript-eslint/no-implicit-any-catch */
